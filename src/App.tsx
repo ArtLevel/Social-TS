92499b607203258107a682794da11cdd66584cc0
ArtLevel <algitIT@rambler.ru>
2023-07-21 18:03:49 +0300

    Lesson 7 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="App">
+			<Header/>
+			<Technologies/>
+		</div>
+	)
+}
+
+const Technologies = () => {
+	return (
+		<div>
+			<ul>
+				<li>css</li>
+				<li>html</li>
+				<li>js</li>
+				<li>react</li>
+			</ul>
+		</div>
+	)
+}
+
+const Header = () => {
+	return (
+		<div>
+			<a href='#'>Home</a>
+			<a href='#'>News Feed</a>
+			<a href='#'>Messages</a>
+		</div>
+	)
 }
 
 export default App;
