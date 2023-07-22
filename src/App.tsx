b6dc00d0cb4679b8a60a90c1bd23189d9f80fd60
ArtLevel <algitIT@rambler.ru>
2023-07-22 14:13:42 +0300

    Lesson 13: Git 1/2, complete

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ const App = () => {
 
 			<Header />
 			<Navbar />
-
 			<Profile />
 
 		</div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
 	return (
 		<header className='header'>
-		<img src='https://cdn.logo.com/hotlink-ok/logo-social.png'/>
-	</header>
+			<img src='https://cdn.logo.com/hotlink-ok/logo-social.png'/>
+		</header>
 	)
 }
 
