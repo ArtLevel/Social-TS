7dcfdbdc8ea853c9301b8a2bf378f68891b84ed6
ArtLevel <algitIT@rambler.ru>
2023-07-22 19:11:25 +0300

    Lesson 16: Props 2/2, complete

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,8 +12,8 @@ const MyPosts = () => {
 			<button>Remove post</button>
 
 			<div className={s.posts}>
-				<Post/>
-				<Post/>
+				<Post message='Hi, how are you ?'/>
+				<Post message="It's my first post"/>
 			</div>
 
 		</div>
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {FC} from 'react';
 
 import s from './Post.module.css';
 
-// Theme props theory
+interface IPost {
+	message: string
+}
 
-const Post = () => {
+const Post: FC<IPost> = ({message}) => {
 	return (
 		<div className={s.item}>
 			<img
 				src='https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'/>
-			post 1
+			{message}
 		</div>
 	)
 }
