9e55f5f1ebefa8fd530713a9e9ca20720415faf4
ArtLevel <algitIT@rambler.ru>
2023-07-22 13:49:13 +0300

    Added Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav className='nav'>
+		<ul>
+			<li>
+				<a>Profile</a>
+			</li>
+			<li>
+				<a>Message</a>
+			</li>
+			<li>
+				<a>News</a>
+			</li>
+			<li>
+				<a>Music</a>
+			</li>
+		</ul>
+	</nav>
+	)
+}
+
+export default Navbar
