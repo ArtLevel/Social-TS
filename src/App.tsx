a9bbc6a13afed2580d64343b39e56c9bebe7ae15
ArtLevel <algitIT@rambler.ru>
2023-07-22 14:36:29 +0300

    Lesson 13: Git 2/2, complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import './App.css';
 import Navbar from './components/NavBar';
 import Profile from './components/Profile';
 
+// Lesson of Git
+// gitk --all& (открыть графическую оболочку гита, чтобы позырить все коммиты)
+// ctrl + insert (копировать в буфер обмена)
+// shift + insert (вставить из буфера обмена)
+// git checkout commit-number (переключится на нужный коммит)
+
 const App = () => {
 	return (
 		<div className="app-wrapper">
