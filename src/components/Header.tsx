ecb8bd1c3d4e2fb82720679dc29e596fc923b6f8
ArtLevel <algitIT@rambler.ru>
2023-07-22 13:47:25 +0300

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+		<img src='https://cdn.logo.com/hotlink-ok/logo-social.png'/>
+	</header>
+	)
+}
+
+export default Header
