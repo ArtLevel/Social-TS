e4812d8140c189e8e2d8c70ec72213272240df90
ArtLevel <algitIT@rambler.ru>
2023-07-28 19:37:29 +0300

    Lesson 21 complete

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,10 +2,23 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
 	return (
-		<div>
-			<h1>
-				Dialogs
-			</h1>
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog + ' ' + s.active}>
+					Dimych
+				</div>
+				<div className={s.dialog}>
+					Andrey
+				</div>
+				<div className={s.dialog}>
+					Sveta
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>Yo</div>
+				<div className={s.message}>Hello !!!</div>
+			</div>
 		</div>
 	)
 }
