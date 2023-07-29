8477cdcc4218b1add836047a7a239a6704d648cb
ArtLevel <algitIT@rambler.ru>
2023-07-29 12:50:07 +0300

    2 week complete

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import {Post} from './Post/Post';
 
+// 2 week complete
+
 export const MyPosts = () => {
 	return (
 		<div className={s.postsBlock}>
