bdf5b1812a2b38bce831e643a18f39bdb8f0861a
ArtLevel <algitIT@rambler.ru>
2023-07-29 14:21:31 +0300

    lesson 26: 2/2, complete

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,27 +1,19 @@
-import React from 'react';
+import React, {FC} from 'react';
 
 import s from './MyPosts.module.css';
 import {Post} from './Post/Post';
 
-export const MyPosts = () => {
-	const postData = [
-		{
-			id: 1,
-			message: 'Hi, how are you ?',
-			likesCount: 12
-		},
-		{
-			id: 2,
-			message: 'Yo !',
-			likesCount: 30
-		},
-		{
-			id: 3,
-			message: 'It\'s my first post',
-			likesCount: 120
-		}
-	]
+type PostDataType = {
+	id: number
+	message: string
+	likesCount: number
+}
 
+interface IMyPostsProps {
+	postData: PostDataType[]
+}
+
+export const MyPosts: FC<IMyPostsProps> = ({postData}) => {
 	const postEl = postData.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
 
 	return (
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,11 +5,29 @@ import {MyPosts} from './MyPosts/MyPosts';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 
 export const Profile = () => {
+	const postData = [
+		{
+			id: 1,
+			message: 'Hi, how are you ?',
+			likesCount: 12
+		},
+		{
+			id: 2,
+			message: 'Yo !',
+			likesCount: 30
+		},
+		{
+			id: 3,
+			message: 'It\'s my first post',
+			likesCount: 120
+		}
+	]
+
 	return (
 		<div>
 
 			<ProfileInfo/>
-			<MyPosts/>
+			<MyPosts postData={postData}/>
 
 		</div>
 	)
