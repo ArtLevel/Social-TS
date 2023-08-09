bc89d77273f7c78059a082a176d1d6285fa9f1a4
ArtLevel <algitIT@rambler.ru>
2023-08-09 13:36:27 +0300

    lesson 32 complete: 2/2

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,10 @@
 import React, {FC} from 'react';
 
-import s from './Profile.module.css';
 import {MyPosts} from './MyPosts/MyPosts';
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 import {PostType} from '../../types/types';
+
+import s from './Profile.module.css';
 
 interface IProfile {
 	posts: PostType[]
