fddd4833f59046596162f701935edcd3803e45dc
ArtLevel <algitIT@rambler.ru>
2023-08-20 18:05:59 +0300

    Refactor code

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -4,20 +4,24 @@ const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
 const dialogsReducer = (state: DialogsPageType, action: ActionType) => {
-	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-		if (action.newText !== undefined) {
-			state.newMessageBody = action.newText
+	switch (action.type) {
+		case UPDATE_NEW_MESSAGE_BODY: {
+			if (action.newText !== undefined) {
+				state.newMessageBody = action.newText
+			}
+			return state
+		}
+		case SEND_MESSAGE: {
+			const body = state.newMessageBody
+			state.messages.push({ id: 4, message: body })
+
+			state.newMessageBody = ''
+			return state
+		}
+		default: {
+			return state
 		}
 	}
-
-	if (action.type === SEND_MESSAGE) {
-		const body = state.newMessageBody
-		state.messages.push({ id: 4, message: body })
-
-		state.newMessageBody = ''
-	}
-
-	return state
 }
 
 export default dialogsReducer
--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -4,24 +4,28 @@ const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
 const profileReducer = (state: ProfilePageType, action: ActionType) => {
-	if (action.type === ADD_POST) {
-		const newPost: PostType = {
-			id: 5,
-			message: state.newPostText,
-			likesCount: 0
+	switch (action.type) {
+		case ADD_POST: {
+			const newPost: PostType = {
+				id: 5,
+				message: state.newPostText,
+				likesCount: 0
+			}
+
+			state.posts.push(newPost)
+			state.newPostText = ''
+			return state
 		}
-
-		state.posts.push(newPost)
-		state.newPostText = ''
-	}
-
-	if (action.type === UPDATE_NEW_POST_TEXT) {
-		if (action.newText !== undefined) {
-			state.newPostText = action.newText
+		case UPDATE_NEW_POST_TEXT: {
+			if (action.newText !== undefined) {
+				state.newPostText = action.newText
+			}
+			return state
+		}
+		default: {
+			return state
 		}
 	}
-
-	return state
 }
 
 export default profileReducer
