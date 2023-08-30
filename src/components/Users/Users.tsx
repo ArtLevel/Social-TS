c8c3cceb4469732a47133a040565844ab3acea0d
ArtLevel <algitIT@rambler.ru>
2023-08-30 20:56:06 +0300

    Users refactor

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,9 +6,45 @@ interface IUsers {
 	users: UsersType[]
 	unfollow: (userId: number) => void
 	follow: (userId: number) => void
+	setUsers: (users: UsersType[]) => void
 }
 
-export const Users: FC<IUsers> = ({ users, unfollow, follow }) => {
+export const Users: FC<IUsers> = ({ users, unfollow, follow, setUsers }) => {
+	!users.length && setUsers([
+		{
+			id: 1,
+			photoUrl: 'https://img.freepik.com/premium-vector/mans-head-avatar-vector_83738-354.jpg?w=2000',
+			fullName: 'Dmitry',
+			status: 'A am a boss',
+			followed: true,
+			location: {
+				city: 'Minsk',
+				country: 'Belarus'
+			}
+		},
+		{
+			id: 2,
+			photoUrl: 'https://img.freepik.com/premium-vector/mans-head-avatar-vector_83738-354.jpg?w=2000',
+			fullName: 'Sasha',
+			status: 'A am a boss, too',
+			followed: false,
+			location: {
+				city: 'Moscow',
+				country: 'Russia'
+			}
+		},
+		{
+			id: 3,
+			photoUrl: 'https://img.freepik.com/premium-vector/mans-head-avatar-vector_83738-354.jpg?w=2000',
+			fullName: 'Andrew',
+			status: 'A am a boss, too',
+			followed: true,
+			location: {
+				city: 'Kiev',
+				country: 'Ukraine'
+			}
+		}])
+
 	const onUnfollowHandler = (userId: number) => unfollow(userId)
 	const onFollowHandler = (userId: number) => follow(userId)
 
