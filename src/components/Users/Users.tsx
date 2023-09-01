759577afed3e7cdb781be7966c194669501af7a8
ArtLevel <algitIT@rambler.ru>
2023-09-01 15:33:01 +0300

    Refactor code

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,18 +12,16 @@ interface IUserProps {
 }
 
 class User extends React.Component<IUserProps> {
-	getUsers = () => {
-		if (!this.props.users.length) {
-			axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-				this.props.setUsers(response.data.items)
-			})
-		}
+	constructor(props: IUserProps) {
+		super(props)
+		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+			this.props.setUsers(response.data.items)
+		})
 	}
 
 	render() {
 		return (
 			<div>
-				<button onClick={this.getUsers}>Get Users</button>
 				{this.props.users.map(u => (
 					<div key={u.id}>
 		<span>
