a3a9fcd9c8bcc930f116fdab29e49885eb4b2a83
ArtLevel <algitIT@rambler.ru>
2023-09-02 13:22:35 +0300

    Refactor

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,52 @@
+import React from 'react'
 import { connect } from 'react-redux'
+import axios from 'axios'
 import { followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC } from '../../redux/usersReducer'
 import { ActionType, StateType, UserType } from '../../types/types'
-import UsersAPIComponent from './UsersAPIComponent'
+import { Users } from './Users'
+
+interface IUsersAPIComponentProps {
+	users: UserType[]
+	pageSize: number
+	totalUsersCount: number
+	currentPage: number
+
+	follow: (userId: number) => void
+	unfollow: (userId: number) => void
+	setUsers: (users: UserType[]) => void
+	setCurrentPage: (currentPage: number) => void
+	setTotalUsersCount: (totalCount: number) => void
+}
+
+class UsersAPIComponent extends React.Component<IUsersAPIComponentProps> {
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+			this.props.setUsers(response.data.items)
+			this.props.setTotalUsersCount(response.data.totalCount)
+		})
+	}
+
+	onPageChanged = (currentPage: number) => {
+		this.props.setCurrentPage(currentPage)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`).then(response => {
+			this.props.setUsers(response.data.items)
+		})
+	}
+
+	render() {
+		const {
+			users,
+			pageSize,
+			totalUsersCount,
+			currentPage,
+			follow,
+			unfollow
+		} = this.props
+
+		return <Users users={users} pageSize={pageSize} totalUsersCount={totalUsersCount} currentPage={currentPage}
+		              follow={follow} unfollow={unfollow} onPageChanged={this.onPageChanged} />
+	}
+}
 
 const mapStateToProps = (state: StateType) => {
 	return {
