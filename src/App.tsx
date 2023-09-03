5e6b153cb1976e959c8332eab7cf3b69ea91b82c
ArtLevel <algitIT@rambler.ru>
2023-09-03 13:58:55 +0300

    Create ProfileContainer

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Route } from 'react-router-dom'
 
 import { Header } from './components/Header/Header'
 import { NavBar } from './components/NavBar/NavBar'
-import { Profile } from './components/Profile/Profile'
 import './App.css'
 import { DialogsContainer } from './components/Dialogs/DialogsContainer'
 import UsersContainer from './components/Users/UsersContainer'
+import ProfileContainer from './components/Profile/ProfileContainer'
 
 const App = () => {
 	return (
@@ -17,7 +17,7 @@ const App = () => {
 
 			<div className='app-wrapper-content'>
 				<Route path='/dialogs' render={() => <DialogsContainer />} />
-				<Route path='/profile' render={() => <Profile />} />
+				<Route path='/profile' render={() => <ProfileContainer />} />
 				<Route path='/users' render={() => <UsersContainer />} />
 			</div>
 		</div>
---
+++ src/components/Profile/ProfileContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Profile } from './Profile'
+
+class ProfileContainer extends React.Component {
+	render() {
+		return (
+			<Profile />
+		)
+	}
+}
+
+export default ProfileContainer
