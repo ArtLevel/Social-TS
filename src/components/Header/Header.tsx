bbcc867217fad2b56ab70f112456a5f9e46faee4
ArtLevel <algitIT@rambler.ru>
2023-09-13 14:07:54 +0300

    HeaderContainer create

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import s from './Header.module.css'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
 	return (
 		<header className={s.header}>
 			<img src='https://cdn.logo.com/hotlink-ok/logo-social.png' />
+			<div className={s.loginBlock}>
+				<NavLink to='/login'>Login</NavLink>
+			</div>
 		</header>
 	)
 }
---
+++ src/components/Header/HeaderContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from './Header'
+
+class HeaderContainer extends React.Component<any, any> {
+	render() {
+		return <Header />
+	}
+}
+
+export default HeaderContainer
