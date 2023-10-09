52fcba115ec8150804b03fbf4413b8e68d952819
ArtLevel <algitIT@rambler.ru>
2023-10-09 14:16:26 +0300

    Profile Status update

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,16 +1,35 @@
-import React, { FC } from 'react'
+import React from 'react'
 
 interface IProfileStatus {
 	status: string
 }
 
-export const ProfileStatus: FC<IProfileStatus> = ({ status }) => {
-	return <div>
-		<div>
-			<span>{status}</span>
+export class ProfileStatus extends React.Component<IProfileStatus> {
+	state = {
+		editMode: false
+	}
+
+	activateEditMode = () => {
+		this.setState({ editMode: true })
+	}
+
+	deactivateEditMode = () => {
+		this.setState({ editMode: false })
+	}
+
+	render() {
+		return <div>
+			{
+				!this.state.editMode
+					?
+					<div>
+						<span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+					</div>
+					:
+					<div>
+						<input onBlur={this.deactivateEditMode} value={this.props.status} autoFocus />
+					</div>
+			}
 		</div>
-		<div>
-			<input value={status} />
-		</div>
-	</div>
+	}
 }
