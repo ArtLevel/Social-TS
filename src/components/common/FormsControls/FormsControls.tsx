a7416446219f41a4ae89e890724d5fe69afc9358
ArtLevel <algitIT@rambler.ru>
2023-10-12 16:22:26 +0300

    Added typization in the FormsControls

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,8 +1,24 @@
-export const Textarea = ({ input, meta, ...props }: any) => {
-	debugger
+import s from './FormsControls.module.css'
+import { WrappedFieldInputProps, WrappedFieldMetaProps } from 'redux-form'
+import { FC, HTMLInputTypeAttribute } from 'react'
+
+interface ITextarea {
+	meta: WrappedFieldMetaProps,
+	input: WrappedFieldInputProps,
+	placeholder?: string,
+	type?: HTMLInputTypeAttribute,
+	autoFocus?: boolean
+}
+
+export const Textarea: FC<ITextarea> = ({ input, meta, ...props }) => {
+	const hasError = meta.touched && meta.error
+
 	return (
-		<div>
-			<textarea {...input} {...props} />
+		<div className={s.formControl + ' ' + hasError ? s.error : ''}>
+			<div>
+				<textarea {...input} {...props} />
+			</div>
+			{hasError && <span>{'some error'}</span>}
 		</div>
 	)
 }
