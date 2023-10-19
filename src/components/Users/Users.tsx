52a8e06bec5857aca972a5250b0346539bc35060
ArtLevel <algitIT@rambler.ru>
2023-10-19 13:39:15 +0300

    Added User.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,7 @@
 import React, { FC } from 'react'
 import { UserT } from '../../types/Pages/Users/UsersPageT'
-import userPhoto from '../../assets/images/user.png'
-import s from './Users.module.css'
-import { NavLink } from 'react-router-dom'
 import { Paginator } from '../common/Paginator/Paginator'
+import { User } from './User'
 
 interface IUsers {
 	users: UserT[]
@@ -37,31 +35,8 @@ export const Users: FC<IUsers> = (props) => {
 	}
 
 	const usersMapped = users.map(u => (
-		<div key={u.id}>
-		<span>
-			<div>
-				<NavLink to={`/profile/${u.id}`}><img src={u.photos.small ? u.photos.small : userPhoto}
-				                                      className={s.userPhoto} /></NavLink>
-			</div>
-			<div>
-				{u.followed
-					? <button onClick={() => unfollowHandler(u.id)}
-					          disabled={followingInProgress.some(id => id === u.id)}>Unfollow</button>
-					: <button onClick={() => followHandler(u.id)}
-					          disabled={followingInProgress.some(id => id === u.id)}>Follow</button>}
-			</div>
-		</span>
-			<span>
-			<span>
-				<div>{u.name}</div>
-				<div>{u.status}</div>
-			</span>
-			<span>
-				<div>{'u.location.country'}</div>
-				<div>{'u.location.city'}</div>
-			</span>
-		</span>
-		</div>
+		<User key={u.id} followingInProgress={followingInProgress} user={u} followHandler={followHandler}
+		      unfollowHandler={unfollowHandler} />
 	))
 
 	return (
